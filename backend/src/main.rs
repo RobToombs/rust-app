99ff44c7ec754850b8bec89f1e7cb6a526bb8b41
Rob Toombs <rtoombs@shieldsrx.com>
2022-10-31 20:26:40 -0400

    1.2 Hello, World!

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,3 @@
-use actix_web::{App, HttpServer, web};
-use crate::endpoints::{AppState, echo, index, manual_hello};
-
-mod endpoints;
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("Actix Web"),
-            }))
-            .service(index)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+fn main() {
+    println!("Hello, world!");
 }
