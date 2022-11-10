85e9e52c5b60493433afd61335a03941bf5a6b8b
Rob Toombs <rtoombs@shieldsrx.com>
2022-11-09 20:26:34 -0500

    3.2 Data Types - Tuples

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,5 @@
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-
 fn main() {
-    let x = 5;
+    let (x, y, z): (i32, f64, u8) = (500, 6.4, 1);
 
-    let x = x + 1;
-
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-
-    println!("The value of x is: {x}");
+    println!("The value of y is: {y}");
 }
