2db061b15f1c5a0126d229731deef20817b6c2ed
Rob Toombs <rtoombs@shieldsrx.com>
2022-11-14 18:52:20 -0500

    3.2 Data Types - Memory Safety

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,22 @@
-fn main() {
-    let (x, y, z): (i32, f64, u8) = (500, 6.4, 1);
+use std::io;
 
-    println!("The value of y is: {y}");
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
